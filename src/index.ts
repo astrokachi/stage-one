ecd0e3237646a6bdc4cb22c68d9c8749a3608945
astrokachi <astrokachi@gmail.com>
2023-09-10 14:04:09 +0100

    feat: add basic get request

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,42 @@
 import http from "http";
+import url from "url";
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+	const parsedUrl = url.parse(req.url, true);
 
+	if (req.method === "GET" && parsedUrl.pathname === "/") {
+		const { slack_name, track } = parsedUrl.query;
+		const date = new Date();
 
-server.listen(3000, () => {
+		const days = [
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday",
+		];
+
+		const resp = {
+			slack_name,
+			current_day: days[date.getDay()],
+			utc_time: date,
+			track,
+			github_file_url:
+				"https://github.com/astrokachi/stage-one/blob/main/src/index.ts",
+			github_repo_url: "https://github.com/astrokachi/stage-one",
+			status_code: 200,
+		};
+
+		res.writeHead(200, { "Content-Type": "text/plain" });
+		res.end(JSON.stringify(resp));
+	} else {
+		res.writeHead(404, { "Content-Type": "text/plain" });
+		res.end("Not Found\n");
+	}
+});
+
+server.listen(3001, () => {
 	console.log("server is up and running");
 });
